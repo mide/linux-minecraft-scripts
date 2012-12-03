02061819efb83066c4c31184c2ad25dfe6d7b6a3
Goten87 <altogoten@gmail.com>
2012-12-03 04:00:15 +0100

    added mc-backup-cron.sh
    added this file so people will see if its started from a user of the
    automatic backup system is doing it

---
+++ mc-backup-cron.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+source ./config.cfg 
+
+# Minecraft Backup Script                  
+# Author: Mark Ide <cranstonide@gmail.com> 
+# Github: https://github.com/cranstonide/linux-minecraft-scripts
+
+############################################
+# Script Settings                          #
+# (Note -NO- trailing slashes on paths)    #
+############################################
+
+
+############################################
+# End of settings; Do not modify below     #
+############################################
+
+# We need to first put the server in readonly mode to reduce the chance of backing up half of a chunk. 
+screen -p 0 -S minecraft -X eval "stuff \"say Another hour has passed, time for a backup..\"\015"
+screen -p 0 -S minecraft -X eval "stuff \"say "$serverNick" backup system starting up.\"\015"
+screen -p 0 -S minecraft -X eval "stuff \"save-off\"\015"
+screen -p 0 -S minecraft -X eval "stuff \"save-all\"\015"
+
+# Wait a few seconds to make sure that Minecraft has finished backing up.
+sleep 5
+
+# Create a copy for the most recent server image directory (its a convenient way to recover 
+# a single players' data or chunks without unzipping the whole archive). If you don't need a 
+# directory with the most recent image, you may comment this section out.
+rm -rvf $backupDir/$serverNick-most-recent
+mkdir $backupDir/$serverNick-most-recent
+cp -Rv $minecraftDir/* $backupDir/$serverNick-most-recent
+
+# Create an archived copy in .tar.gz format.
+DATE=$(date +%d.%m.%Y-%T)
+rm -rvf $backupDir/$serverNick-$DATE.tar.gz
+tar -cvzf $backupDir/$serverNick-$DATE.tar.gz $minecraftDir/*
+
+sleep 5
+# Don't forget to take the server out of readonly mode.
+screen -p 0 -S minecraft -X eval "stuff \"say "$serverNick" backup system is Setting auto save world on.\"\015"
+screen -p 0 -S minecraft -X eval "stuff \"save-on\"\015"
+sleep 5
+# Wait a second for the gnu-screen to allow another stuffing and optionally alert users that the backup has been completed.
+sleep 1
+screen -p 0 -S minecraft -X eval "stuff \"say "$serverNick" backup system has been completed.\"\015"
+
+# (Optionally) Remove all old (older than 3 days) backups to cut down on disk utilization. 
+find $backupDir* -mtime +3 -exec rm {} -fv \;
