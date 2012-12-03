344855dad4a4ca088a967539da807413757fa1ce
Goten87 <altogoten@gmail.com>
2012-12-03 05:51:20 +0100

    added the config making script to it
    it works i tested it on my server
    if u find a bug or a spelling misstake please let me know

--- install.sh
+++ install.sh
@@ -1,41 +1,96 @@
 #!/bin/bash
-clear
 
-# Script To Make All Files Executable
+# Admin script
 # Author: Remko de Bruin <altogoten@gmail.com> 
 # Github: https://github.com/Goten87/linux-minecraft-scripts
 
+clear
+# Making all the Config files
+echo "#!/bin/bash
+
+# Minecraft Script Configs
+# Author: Remko de Bruin <altogoten@gmail.com> 
+# Github: https://github.com/Goten87/linux-minecraft-scripts" >> ./config.cfg
+
+#Making The Minecraft DIR Config
+read -p " Give your minecraft dir path (directory containing, no trailing slash): 
+ " minecraftDir
+echo "
+# Location of the minecraft_server.jar (directory containing, no trailing slash)
+minecraftDir=$minecraftDir" >> ./config.cfg
+
+#Making The Minecraft Backup DIR Config
+read -p " Give your backup dir path (directory containing, no trailing slash): 
+ " backupDir
+	echo "
+# Where do you wish to store the backups (directory containing, no trailing slash)
+backupDir=$backupDir" >> ./config.cfg
+
+#Making The Minecraft Backup Time Config
+read -p " Give your cron time stamp: " backupTime
+	echo "
+#The time howlong it takes for your cron job to be run
+backupTime=$backupTime" >> ./config.cfg
+
+#Making The Minecraft Jar Config
+read -p " Give your server jar name (default minecraft_server): 
+ " minecraftJar
+	echo "
+#The jar u want to load in default (minecraft_server) or (minecraftforge-universal)
+jar=$minecraftJar" >> ./config.cfg
+
+#Making The Minecraft serverNick Config
+read -p " Give your server name: " serverNick
+	echo "
+# Do you have more than one server? What do you want to call this one?
+serverNick=$serverNick" >> ./config.cfg
+
+#Making The Minecraft tempDir Config
+read -p " Give a temp dir location (directory containing, no trailing slash): 
+ " tempDir
+	echo "
+# Where do you want to store the temporary files? (directory containing, no trailing slash)
+tempDir=$tempDir" >> ./config.cfg
+
+#Making The Minecraft tempDir Config
+read -p " Where is your minecraft log file: 
+ " log
+	echo "
+# Where is your Minecraft Log?
+log=$log" >> ./config.cfg
+
+# Make All Files Executable
 echo " Making admin.sh executable"
-chmod +X ./admin.sh
+chmod +x ./admin.sh
 sleep 1
 echo " Making mc-auth.sh executable"
-chmod +X ./mc-auth.sh
+chmod +x ./mc-auth.sh
 sleep 1
 echo " Making mc-backup.sh executable"
-chmod +X ./mc-backup.sh
+chmod +x ./mc-backup.sh
 sleep 1
 echo " Making mc-backup-cron.sh executable"
-chmod +X ./mc-backup-cron.sh
+chmod +x ./mc-backup-cron.sh
 sleep 1
 echo " Making mc-chat.sh executable"
-chmod +X ./mc-chat.sh
+chmod +x ./mc-chat.sh
 sleep 1
 echo " Making mc-list-all-players.sh executable"
-chmod +X ./mc-list-all-players.sh
+chmod +x ./mc-list-all-players.sh
 sleep 1
 echo " Making mc-restart.sh executable"
-chmod +X ./mc-restart.sh
+chmod +x ./mc-restart.sh
 sleep 1
 echo " Making mc-start.sh executable"
-chmod +X ./mc-start.sh
+chmod +x ./mc-start.sh
 sleep 1
 echo " Making mc-stop.sh executable"
-chmod +X ./mc-stop.sh
+chmod +x ./mc-stop.sh
 sleep 1
 echo " Making mc-temp-creative.sh executable"
-chmod +X ./mc-temp-creative.sh
+chmod +x ./mc-temp-creative.sh
 sleep 1
 echo " Making mc-temp-creative.sh executable"
-chmod +X ./mc-time.sh
+chmod +x ./mc-time.sh
 sleep 1
-read -p " Making executable done press enter to return." dummy
+read -p " Install script is done press enter to return." dummy
